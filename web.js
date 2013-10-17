6edade6691405ccc64b5b3d15d68a0e1b87ca3b2
adaptivedev <adaptivedevs@gmail.com>
2013-10-17 08:11:51 +0300

    mongo

--- web.js
+++ web.js
@@ -10,9 +10,29 @@ var i=0;
 
 app.get('/', function(request, response) {
   i++;
-  response.send('wings: port:' + port + ' access ' + i);
+  var res = 'wings server: port:' + port + ' access ' + i;
+  //res += '<script>window.alert("wings client: access: ' + i + '")</script>';
+  response.send(res);
+
+	var mongo = require('mongodb');
+
+	var mongoUri = process.env.MONGOLAB_URI ||
+	  process.env.MONGOHQ_URL ||
+	  'mongodb://localhost/mydb';
+
+	mongo.Db.connect(mongoUri, function (err, db) {
+	  db.collection('mydocs', function(er, collection) {
+	    //i++;
+	    var res = 'mongo: access ' + i;
+	    console.log(res);
+	    collection.insert({'k': i}, {safe: true}, function(er,rs) {
+	    });
+	  });
+	});
+
 });
 
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
